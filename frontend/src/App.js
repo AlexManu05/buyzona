1233c8bc27d5773b31e82bf1c0aa666e7d579aa3
AlexManu05 <AlexManu05@users.noreply.github.com>
2022-11-03 16:34:58 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Buyzona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
